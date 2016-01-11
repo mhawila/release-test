4c0ac4da6c21282158e471022ee2bb20986e6b66
mhawila <a.mhawila@gmail.com>
2016-01-11 17:00:17 +0300

    More stuff into grunt release task

--- Gruntfile.js
+++ Gruntfile.js
@@ -38,6 +38,18 @@ module.exports = function(grunt) {
         tasks: ['jshint:test', 'nodeunit']
       },
     },
+    release: {
+        options: {
+          npm: false,
+          indentation: '\t', 
+          tagMessage: 'Tagging version <%= version %>', //default: 'Version <%= version %>',
+          beforeBump: [], // optional grunt tasks to run before file versions are bumped
+          afterBump: [], // optional grunt tasks to run after file versions are bumped
+          beforeRelease: [], // optional grunt tasks to run after release version is bumped up but before release is packaged
+          afterRelease: [], // optional grunt tasks to run after release is packaged
+          updateVars: [], // optional grunt config objects to update (this will update/set the version property on the object specified)
+        }
+  }
   });
 
   // Default task.
