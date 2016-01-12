a91abec8337874d210a19b6cbb224c844f4960c9
mhawila <a.mhawila@gmail.com>
2016-01-12 12:42:28 +0300

    Modified grunt file

--- Gruntfile.js
+++ Gruntfile.js
@@ -43,8 +43,6 @@ module.exports = function(grunt) {
           npm: false,
           indentation: '\t', 
           tagMessage: 'Tagging version <%= version %>', //default: 'Version <%= version %>',
-          beforeBump: [], // optional grunt tasks to run before file versions are bumped
-          afterBump: [], // optional grunt tasks to run after file versions are bumped
           beforeRelease: [], // optional grunt tasks to run after release version is bumped up but before release is packaged
           afterRelease: [], // optional grunt tasks to run after release is packaged
           updateVars: [], // optional grunt config objects to update (this will update/set the version property on the object specified)
